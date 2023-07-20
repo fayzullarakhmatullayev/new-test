d2e4c0ece83dc14b4c4bc0bdc2a98a2eb336ba1b
fayzullarakhmatullayev <fayzullarakhmatullayev@gmail.com>
2023-07-20 12:59:58 +0500

    finish

---
+++ src/components/TopLeftBlock.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="top__block">
+    <div class="item" v-for="item in items" :key="item.id">{{ item.name }}</div>
+
+    <span>selected: N / M</span>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    default: () => []
+  }
+})
+</script>
